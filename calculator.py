0d477d4825c9d224d2935678c6d38710da907b67
warnec <warnec@oregonstate.edu>
2021-02-04 08:35:26 -0800

    first rough working calculator

---
+++ calculator.py
@@ -0,0 +1,27 @@
+
+
+
+
+def add(x,y):
+    result = x + y
+    return result
+
+def subtract(x,y):
+    return x - y
+
+def multiply(x,y):
+    return x * y
+
+def divide(x,y):
+    if y == 0:
+        print("The denominator can not be zero!")
+        return None
+    return x / float(y)
+
+userX = input('Enter the first number: ')
+userY = input('Enter the second number: ')
+
+print("The numbers added = ", add(int(userX), int(userY)))
+print("The numbers subtracted = ", subtract(int(userX), int(userY)))
+print("The numbers multiplied = ", multiply(int(userX), int(userY)))
+print("The numbers divided = ", divide(float(userX), float(userY)))
