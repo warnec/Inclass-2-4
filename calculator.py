c0084a2f0b631da8d215fc7fa8e7702bde2499e3
warnec <warnec@oregonstate.edu>
2021-02-04 08:38:23 -0800

    Added user input error catching

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 
-
+#Colston Warne
+#Inclass assignment 1
 
 
 def add(x,y):
@@ -20,8 +21,11 @@ def divide(x,y):
 
 userX = input('Enter the first number: ')
 userY = input('Enter the second number: ')
+try:
+    print("The numbers added = ", add(int(userX), int(userY)))
+    print("The numbers subtracted = ", subtract(int(userX), int(userY)))
+    print("The numbers multiplied = ", multiply(int(userX), int(userY)))
+    print("The numbers divided = ", divide(float(userX), float(userY)))
 
-print("The numbers added = ", add(int(userX), int(userY)))
-print("The numbers subtracted = ", subtract(int(userX), int(userY)))
-print("The numbers multiplied = ", multiply(int(userX), int(userY)))
-print("The numbers divided = ", divide(float(userX), float(userY)))
+except ValueError:
+    print("You must enter integer values!")
